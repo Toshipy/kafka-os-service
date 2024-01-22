a988808c2cdcc0837ce70daee510fb7857ff0098
arai <arai@tagbangers.co.jp>
2024-01-22 23:12:40 +0900

    initial commit

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public static void contextLoad() {
+
+    }
+}
